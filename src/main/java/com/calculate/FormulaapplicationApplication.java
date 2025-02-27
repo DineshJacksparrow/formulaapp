a7b32e25e2cde4168d5106fd7860f78f547345b5
RedwoodTechno <consultantdineshkumar@gmail.com>
2025-02-27 14:08:07 +0530

    first commit

---
+++ src/main/java/com/calculate/FormulaapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.calculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculate/FormulaapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
